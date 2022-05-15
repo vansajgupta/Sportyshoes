31fb152d1e9d021718803b0aa6aa112badbd6183
vansajgupta <102907366+vansajgupta@users.noreply.github.com>
2022-05-15 13:08:04 +0530

    sporty8

---
+++ AuthService.java
@@ -0,0 +1,11 @@
+package com.spring.foobar.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+	public boolean signInUser(String CustomerNumber, String Phone) {
+		return false;
+	}
+}
